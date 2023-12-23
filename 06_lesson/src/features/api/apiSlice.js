4cdffedb8691f1fff72cd57b043c087fcdcfe8ec
Andre Wang on his desktop <andrewang87@yahoo.com.au>
2023-12-23 16:49:50 +1100

    finished intro to rtk query lesson 6

--- 06_lesson/src/features/api/apiSlice.js
+++ 06_lesson/src/features/api/apiSlice.js
@@ -1,16 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+// the tags are important for caches.
+// if you don't have them, results get cached and previous caches do not get invalidated.
+
+// it is not updating to show new changes delete, update etc. sicne we only see cached data.
+// We must assign a tag to the cache, then let it know what mutations invalidate the cahce,
+// so it will automatically refecth the data for us, and cause re render.
 export const apiSlice = createApi({
-    reducerPath: 'api',
+    reducerPath: 'api', //name
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
     tagTypes: ['Todos'],
     endpoints: (builder) => ({
         getTodos: builder.query({
             query: () => '/todos',
-            transformResponse: res => res.sort((a, b) => b.id - a.id),
+            transformResponse: res => res.sort((a, b) => b.id - a.id), // an option to let us sort. Like
             providesTags: ['Todos']
         }),
-        addTodo: builder.mutation({
+        addTodo: builder.mutation({ // mutation, not query, because we change.
             query: (todo) => ({
                 url: '/todos',
                 method: 'POST',
@@ -38,8 +44,8 @@ export const apiSlice = createApi({
 })
 
 export const {
-    useGetTodosQuery,
+    useGetTodosQuery, // RTK query creates custom hooks based on our name getTodos
     useAddTodoMutation,
     useUpdateTodoMutation,
     useDeleteTodoMutation
-} = apiSlice
+} = apiSlice // our hooks we can export.
